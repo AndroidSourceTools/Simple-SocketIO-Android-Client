b3ba9995bb4f54b87562bba8905a540664925d83
Randy <o1984531@gmail.com>
2019-01-11 17:40:09 +0800

    [Add] Add the application class

--- app/src/main/java/com/socketio/test/SocketIoApplication.java
+++ app/src/main/java/com/socketio/test/SocketIoApplication.java
@@ -1,6 +1,11 @@
 package com.socketio.test;
 
+import android.app.Activity;
 import android.app.Application;
+import android.os.Bundle;
+
+import com.socketio.test.activity.MainActivity;
+import com.socketio.test.utils.SocketIOManager;
 
 public class SocketIoApplication extends Application {
     @Override
